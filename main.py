9849a69f59e116a3fd0c748e59490f92918971b9
KLZ-0 <adrian@kalazi.com>
2021-11-14 11:18:23 +0100

    Add basic argument parsing

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import argparse
+
 import figures
+import straw
+
 
 if __name__ == '__main__':
-    figures.generate_all()
+    parser = argparse.ArgumentParser(description="Lossless multi-channel audio codec")
+    parser.add_argument("-i", "--input", dest="input_files", metavar="INPUT_FILE", type=str, nargs="+",
+                        help="Input files")
+    parser.add_argument("-o", "--output", dest="output_file", metavar="OUTPUT_FILE", type=str,
+                        help="Output file")
+    parser.add_argument("-d", "--decode", dest="decode", action="store_true",
+                        help="Decode")
+
+    # TODO: remove this if done with test runs
+    parser.add_argument("--figures", dest="figures", action="store_true",
+                        help="Generate figures - temporary option for runs with non-consistent behavior")
+
+    args = parser.parse_args()
+
+    if args.figures:
+        figures.generate_all()
+    else:
+        straw.run(args)
+
