4038595df22facb9d7b26cd8099f7adbb6a0d52e
KLZ-0 <adrian@kalazi.com>
2021-11-16 12:58:28 +0100

    Add basic structure for encoder

---
+++ straw/encoder.py
@@ -0,0 +1,15 @@
+class Encoder:
+    def __init__(self):
+        pass
+
+    def load_file(self, filename):
+        pass
+
+    def load_stream(self, stream, samplerate):
+        pass
+
+    def encode(self):
+        pass
+
+    def save_file(self, filename):
+        pass
