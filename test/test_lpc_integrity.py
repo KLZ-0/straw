fe9a5714baa2ae482eb2dee6b98a49a50443f366
Adrián Kálazi <adrian@kalazi.com>
2021-11-30 14:41:52 +0100

    CI setup (#6)
    * Add test workflow
    + Modify tests to not depend on external files
    
    * Add install missing library

--- test/test_lpc_integrity.py
+++ test/test_lpc_integrity.py
@@ -1,24 +1,19 @@
 import unittest
 
-import soundfile
-
 from src.straw import lpc
+from test import get_signal_float, get_signal
 
 
 class LPCSignalIntegrity(unittest.TestCase):
     def test_reconstruction(self):
-        data, sr = soundfile.read("../inputs/maskoff_tone.wav")
+        data, sr = get_signal_float()
 
         lpc_order = 8
         lpc_precision = 12  # bits
 
-        bs = int(sr * 0.020)
-        start = 400
+        qlp, quant_level = lpc.compute_qlp(data, lpc_order, lpc_precision)
 
-        qlp, quant_level = lpc.compute_qlp(data[start:start + bs], lpc_order, lpc_precision)
-
-        data, sr = soundfile.read("../inputs/maskoff_tone.wav", dtype="int16")
-        signal = data[start:start + bs]
+        signal, sr = get_signal()
 
         residual = lpc.compute_residual(signal, qlp, lpc_order, quant_level)
         restored = lpc.restore_signal(residual, qlp, lpc_order, quant_level, signal[:lpc_order])
