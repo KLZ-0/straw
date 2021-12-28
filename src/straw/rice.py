d2fbd9a2ee9cb539084e8c6d6c71894309214770
KLZ-0 <adrian@kalazi.com>
2021-12-28 19:18:01 +0100

    Add comments

--- src/straw/rice.py
+++ src/straw/rice.py
@@ -3,16 +3,32 @@ from bitarray import bitarray
 
 
 class Ricer:
+    """
+    Rice encoder/decoder
+    Currently only supports memory for for memory efficiency comparisons and benchmarks
+    """
+
     def __init__(self, m):
         self.m = m
         self.k = np.log2(self.m)
         self.data = bitarray()
 
     def _append_n_bits(self, number, n):
+        """
+        Appends the n bits of number to the end of the current bitstream
+        :param number: number to append
+        :param n: number of bits to append
+        :return: None
+        """
         for i in range(n):
             self.data.append(number >> (n - i - 1) & 1)
 
     def encode_single(self, s):
+        """
+        Encodes a single number s and appends it to the end of the current bitstream
+        :param s: number to encode
+        :return: None
+        """
         # Quotient code
         q = s // self.m
         r = s % self.m
@@ -29,3 +45,11 @@ class Ricer:
             self._append_n_bits(r, b)
         else:
             self._append_n_bits(r + tmp, b + 1)
+
+    def get_size_bits_unaligned(self):
+        """
+        Size of the current bitstream
+        can be used for benchmarks
+        :return: number of raw bits in the current bitstream
+        """
+        return len(self.data)
