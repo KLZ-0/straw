43600bf4e20ef38a6e29d9b4b9fefc5de2871902
KLZ-0 <adrian@kalazi.com>
2021-12-31 00:07:43 +0100

    Add tests

--- test/test_rice.py
+++ test/test_rice.py
@@ -1,24 +1,41 @@
 import unittest
 
+import numpy as np
+
 from straw import rice
+from test import resources
 from test.resources import rice_str
 
 
 class Rice(unittest.TestCase):
-    def test_encoding_integrity(self):
+    r = rice.Ricer(4)
+
+    def test_rice_ext_arange(self):
         """
         Rice parameter 4
         Encode bitstream of numbers from 0 up to 20
         :return:
         """
-        r = rice.Ricer(4)
-        r_o = ""
+        frame = np.arange(20)
 
-        for i in range(20):
-            r.encode_single(i)
+        r_o = ""
+        for i in frame:
             r_o += rice_str(i, 4)
 
-        self.assertEqual(r.data.to01(), r_o)
+        data = self.r.frame_to_bitstream(frame)
+
+        self.assertEqual(data.to01(), r_o)
+
+    def test_rice_ext_signal(self):
+        frame = resources.get_signal()[0]
+
+        r_o = ""
+        for i in frame:
+            r_o += rice_str(i, 4)
+
+        data = self.r.frame_to_bitstream(frame)
+
+        self.assertEqual(data.to01(), r_o)
 
 
 if __name__ == '__main__':
