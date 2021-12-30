aef15fde65d8f8d632d00da4927fdc2a69d3d400
KLZ-0 <adrian@kalazi.com>
2021-12-30 23:51:04 +0100

    Remove single encode

--- src/straw/ext/rice_encode.pyx
+++ src/straw/ext/rice_encode.pyx
@@ -7,25 +7,6 @@ from bitarray import bitarray
 def append_n_bits(bits: bitarray, number: cython.int, n: cython.int):
     bits.extend([number >> (n - i - 1) & 1 for i in range(n)])
 
-@cython.cdivision(True)
-def encode(bits: bitarray, s: cython.int, m: cython.int, k: cython.int):
-    cdef int q, r, tmp
-
-    # Quotient code
-    q = s / m
-    r = s % m
-
-    for _ in range(q):
-        bits.append(1)
-    bits.append(0)
-
-    # Remainder code
-    tmp = (1 << k + 1) - m
-
-    if r < tmp:
-        append_n_bits(bits, r, k)
-    else:
-        append_n_bits(bits, r + tmp, k + 1)
 
 @cython.cdivision(True)
 def encode_frame(bits: bitarray, nparr: np.array, m: cython.int, k: cython.int):
