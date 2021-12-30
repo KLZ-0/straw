dd6ac38939682dcddefcbc513c26cb1721c14301
KLZ-0 <adrian@kalazi.com>
2021-12-30 14:37:06 +0100

    Print times to stderr

--- src/straw/__init__.py
+++ src/straw/__init__.py
@@ -2,10 +2,27 @@ from .encoder import Encoder
 
 
 def run(args):
+    import timeit, sys
     print(args)
 
     e = Encoder()
+
+    start = timeit.default_timer()
     e.load_files(args.input_files)
+    stop = timeit.default_timer()
+    print(f"<TIME> load_files: {stop - start}", file=sys.stderr)
+
+    start = timeit.default_timer()
     e.create_frames()
+    stop = timeit.default_timer()
+    print(f"<TIME> create_frames: {stop - start}", file=sys.stderr)
+
+    start = timeit.default_timer()
     e.encode()
+    stop = timeit.default_timer()
+    print(f"<TIME> encode: {stop - start}", file=sys.stderr)
+
+    start = timeit.default_timer()
     e.save_file(args.output_file)
+    stop = timeit.default_timer()
+    print(f"<TIME> save_file: {stop - start}", file=sys.stderr)
