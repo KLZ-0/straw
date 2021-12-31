76c1256934e3e1b52da775e0d397c3bde9248f12
KLZ-0 <adrian@kalazi.com>
2021-12-31 13:46:26 +0100

    Add total time

--- src/straw/__init__.py
+++ src/straw/__init__.py
@@ -12,17 +12,19 @@ def run(args):
     stop = timeit.default_timer()
     print(f"<TIME> load_files: {stop - start}", file=sys.stderr)
 
-    start = timeit.default_timer()
+    mid = timeit.default_timer()
     e.create_frames()
     stop = timeit.default_timer()
-    print(f"<TIME> create_frames: {stop - start}", file=sys.stderr)
+    print(f"<TIME> create_frames: {stop - mid}", file=sys.stderr)
 
-    start = timeit.default_timer()
+    mid = timeit.default_timer()
     e.encode()
     stop = timeit.default_timer()
-    print(f"<TIME> encode: {stop - start}", file=sys.stderr)
+    print(f"<TIME> encode: {stop - mid}", file=sys.stderr)
 
-    start = timeit.default_timer()
+    mid = timeit.default_timer()
     e.save_file(args.output_file)
     stop = timeit.default_timer()
-    print(f"<TIME> save_file: {stop - start}", file=sys.stderr)
+    print(f"<TIME> save_file: {stop - mid}", file=sys.stderr)
+
+    print(f"<TIME> total: {stop - start:.3f} seconds", file=sys.stderr)
