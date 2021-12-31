6559364ff26a1eb13d1f0a48a30ea21f210ab484
KLZ-0 <adrian@kalazi.com>
2021-12-31 14:50:17 +0100

    Use cython memoryview

--- src/straw/rice/rice_encode.pyx
+++ src/straw/rice/rice_encode.pyx
@@ -1,10 +1,9 @@
 # distutils: language = c
 # cython: language_level=3
 import cython
-import numpy as np
 from bitarray import bitarray
 
-def append_n_bits(bits: bitarray, number: cython.int, n: cython.int):
+def append_n_bits(bits: bitarray, int number, int n):
     """
     Appends the n bits of number to the end of the current bitstream
     :param bits: bitaray to which the bits will be appended
@@ -15,7 +14,7 @@ def append_n_bits(bits: bitarray, number: cython.int, n: cython.int):
     bits.extend([number >> (n - i - 1) & 1 for i in range(n)])
 
 @cython.cdivision(True)
-def encode_frame(bits: bitarray, frame: np.array, m: cython.int, k: cython.int):
+def encode_frame(bits: bitarray, int[:] frame, int m, int k):
     """
     Encodes a whole residual frame and appends it to the end of the current bitstream
     :param bits: bitaray to which the bits will be appended
@@ -24,9 +23,13 @@ def encode_frame(bits: bitarray, frame: np.array, m: cython.int, k: cython.int):
     :param k: rice k constant
     :return: None
     """
-    cdef int s, q, r, tmp
+    cdef int q, r, tmp, s
+    cdef Py_ssize_t x_max, i
+    x_max = frame.shape[0]
 
-    for s in frame:
+    for i in range(x_max):
+        s = frame[i]
+
         # Quotient code
         q = s / m
         r = s % m
