428e5838e18869ae6d80e7ec76afc2ddfc4aaf9a
KLZ-0 <adrian@kalazi.com>
2021-12-31 10:16:45 +0100

    Comment and refactor

--- src/straw/rice/rice_encode.pyx
+++ src/straw/rice/rice_encode.pyx
@@ -5,14 +5,28 @@ import numpy as np
 from bitarray import bitarray
 
 def append_n_bits(bits: bitarray, number: cython.int, n: cython.int):
+    """
+    Appends the n bits of number to the end of the current bitstream
+    :param bits: bitaray to which the bits will be appended
+    :param number: number to append
+    :param n: number of bits to append
+    :return: None
+    """
     bits.extend([number >> (n - i - 1) & 1 for i in range(n)])
 
-
 @cython.cdivision(True)
-def encode_frame(bits: bitarray, nparr: np.array, m: cython.int, k: cython.int):
+def encode_frame(bits: bitarray, frame: np.array, m: cython.int, k: cython.int):
+    """
+    Encodes a whole residual frame and appends it to the end of the current bitstream
+    :param bits: bitaray to which the bits will be appended
+    :param frame: the frame to be encoded
+    :param m: rice m constant
+    :param k: rice k constant
+    :return: None
+    """
     cdef int s, q, r, tmp
 
-    for s in nparr:
+    for s in frame:
         # Quotient code
         q = s / m
         r = s % m
