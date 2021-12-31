808feef976bd4d2bfcd90354b8fb20f6d1162356
KLZ-0 <adrian@kalazi.com>
2021-12-31 13:43:40 +0100

    Add tests

--- test/test_lpc_integrity.py
+++ test/test_lpc_integrity.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import pandas as pd
 
 from straw import lpc
@@ -7,21 +8,36 @@ from . import resources
 
 
 class LPCSignalIntegrity(unittest.TestCase):
+    lpc_order = 8
+    lpc_precision = 12  # bits
+
+    signal, sr = resources.get_signal()
+    signal_zeros = np.zeros(2 ** 12)
+
     def test_reconstruction(self):
-        signal, sr = resources.get_signal()
-
-        lpc_order = 8
-        lpc_precision = 12  # bits
-
-        df = pd.DataFrame({"frame": [signal]})
-        qlp, shift = lpc.compute_qlp(df.loc[0], lpc_order, lpc_precision)
+        df = pd.DataFrame({"frame": [self.signal]})
+        qlp, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
         df["qlp"] = [qlp]
         df["shift"] = [shift]
 
-        residual = lpc.compute_residual(df.loc[0], lpc_order)
-        restored = lpc.restore_signal(residual, qlp, lpc_order, shift, signal[:lpc_order])
+        residual = lpc.compute_residual(df.loc[0], self.lpc_order)
+        restored = lpc.restore_signal(residual, qlp, self.lpc_order, shift, self.signal[:self.lpc_order])
 
-        self.assertEqual((signal - restored).any(), False)
+        self.assertEqual((self.signal - restored).any(), False)
+
+    def test_zeroframe_qlp(self):
+        df = pd.DataFrame({"frame": [self.signal_zeros]})
+        qlp, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
+        self.assertIsNone(qlp)
+        self.assertEqual(shift, 0)
+
+    def test_zeroframe_residual(self):
+        df = pd.DataFrame({"frame": [self.signal_zeros]})
+        df["qlp"] = [None]
+        df["shift"] = [0]
+
+        residual = lpc.compute_residual(df.loc[0], self.lpc_order)
+        self.assertIsNone(residual)
 
 
 if __name__ == '__main__':
