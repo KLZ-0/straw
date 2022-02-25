4892dd72f08c0f69e1e7861a6cd6ef96c2824906
KLZ-0 <adrian@kalazi.com>
2022-02-25 17:20:41 +0100

    Fix encoding of negative numbers

--- src/straw/rice/rice_encode.pyx
+++ src/straw/rice/rice_encode.pyx
@@ -3,6 +3,19 @@
 import cython
 from bitarray import bitarray
 
+def _interleave(short x):
+    """
+    Implementation of the overlap and interleave scheme from https://en.wikipedia.org/wiki/Golomb_coding
+    :param x: number to be remapped
+    :return: positive integer which can be encoded
+    """
+    if x > 0:
+        return 2 * x
+    elif x < 0:
+        return -2 * x - 1
+    else:
+        return 0
+
 def append_n_bits(bits: bitarray, short number, short n):
     """
     Appends the n bits of number to the end of the current bitstream
@@ -28,7 +41,7 @@ def encode_frame(bits: bitarray, short[:] frame, short m, short k):
     x_max = frame.shape[0]
 
     for i in range(x_max):
-        s = frame[i]
+        s = _interleave(frame[i])
 
         # Quotient code
         q = s / m
