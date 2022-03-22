a27d6e3afd9dba64906b89854578dd6cd49ad660
KLZ-0 <adrian@kalazi.com>
2022-03-22 17:36:48 +0100

    Add note

--- src/figures/experiments.py
+++ src/figures/experiments.py
@@ -19,6 +19,9 @@ def func(df):
     mid = variances.mean()
     mid_idx = np.abs(variances - mid).idxmin()
 
+    # NOTE: for some reason subtracting the weakest channel does the smallest harm
+    # mid_idx = variances.idxmin()
+
     df["residual"] = df["residual"].apply(sub, x2=df["residual"][mid_idx])
     # df["residual"] = df["residual"].apply(np.subtract, x2=df["residual"][mid_idx])
 
