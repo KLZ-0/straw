2993ba0f358d9ae5f1d23392ab7a211720ef9f63
KLZ-0 <adrian@kalazi.com>
2022-04-02 23:14:40 +0200

    Fix tests

--- test/test_lpc_integrity.py
+++ test/test_lpc_integrity.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 from straw import lpc
+from straw.lpc.steps import restore_signal
 from . import resources
 
 
@@ -16,18 +17,18 @@ class LPCSignalIntegrity(unittest.TestCase):
 
     def test_reconstruction(self):
         df = pd.DataFrame({"frame": [self.signal]})
-        qlp, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
+        qlp, precision, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
         df["qlp"] = [qlp]
         df["shift"] = [shift]
 
         df = lpc.compute_residual(df.loc[0])
-        restored = lpc.steps.restore_signal(df["residual"], qlp, shift, self.signal[:self.lpc_order])
+        restored = restore_signal(df["residual"], qlp, shift, self.signal[:self.lpc_order])
 
         self.assertEqual((self.signal - restored).any(), False)
 
     def test_zeroframe_qlp(self):
         df = pd.DataFrame({"frame": [self.signal_zeros]})
-        qlp, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
+        qlp, precision, shift = lpc.compute_qlp(df.loc[0], self.lpc_order, self.lpc_precision)
         self.assertIsNone(qlp)
         self.assertEqual(shift, 0)
 
