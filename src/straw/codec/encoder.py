22af3e45a5d0e556f2dbfb9371d097c513f91fe9
KLZ-0 <adrian@kalazi.com>
2022-04-04 19:29:51 +0200

    Save testing env

--- src/straw/codec/encoder.py
+++ src/straw/codec/encoder.py
@@ -62,14 +62,15 @@ class Encoder(BaseCoder):
         """
         self._parametrize()
         lpc_frames = self._set_frame_types()
-        # self._apply_corrections()
+        # self._apply_corrections()  # 0, 1, 2, 3
+        # self._deconvolve_signals()  # 1
         tmp = self._data[lpc_frames].groupby("seq").apply(lpc.compute_qlp, self._lpc_order, self._lpc_precision)
         self._data[["qlp", "qlp_precision", "shift"]] = tmp
-        # self._deconvolve_signals()
+        # self._deconvolve_signals()  # 2
         self._data = self._data.groupby("seq").apply(lpc.compute_residual)
-        # self._apply_corrections("residual")
         self._data["bps"] = np.full(len(self._data["residual"]), 4, dtype="B")
-        # self._deconvolve_signals("residual")
+        self._apply_corrections("residual")  # 4
+        self._deconvolve_signals("residual")  # 3, 4
         self._data["stream"] = self._ricer.frames_to_bitstreams(self._data, parallel=True)
         self._data["stream_len"] = self._data["stream"].apply(len)
         self._ensure_compression()
@@ -81,8 +82,9 @@ class Encoder(BaseCoder):
         :return: None
         """
         self._print_var(seq=4)
-        # show_frame(self._data[self._data["seq"] == 4], terminate=False, limit=(1740, 1800), file_name="gain_shift_correction_after.png")
-        # show_frame(self._data[self._data["seq"] == 4], col_name="residual", limit=(1740, 1800))
+        # exp = 4
+        # show_frame(self._data[self._data["seq"] == 4], terminate=False, limit=(1750, 1810), file_name=f"{exp}_frame.png")
+        # show_frame(self._data[self._data["seq"] == 4], terminate=False, col_name="residual", file_name=f"{exp}_residual.png", limit=(1740, 1800))
         # show_frame(self._data[self._data["seq"] == 4], terminate=False, file_name="gain_shift_correction_after.png")
         # show_frame(self._data[self._data["seq"] == 4], col_name="residual")
         Formatter().save(self._data, self._params, output_file, self._flac_mode)
@@ -152,18 +154,20 @@ class Encoder(BaseCoder):
 
     def _print_var(self, seq=0):
         old_maxabs = np.asarray([352, 373, 581, 516, 432, 349, 380, 391])
-        nocorr_var = np.asarray([1180.79508441, 1856.41195693, 3204.70594336, 2090.79251358, 1489.54344976,
-                                 1078.56373649, 1556.83422256, 2079.87009688])
+        nocorr_var = np.asarray([10997.481, 24395.01, 50948.516, 36896.603, 21682.603, 11630.761,
+                                 14912.361, 18267.361])
         self._tmp(seq, np.var, "var", nocorr_var)
         self._tmp(seq, lambda x: np.max(np.abs(x)), "absmax", old_maxabs)
 
     def _tmp(self, seq, func, name, old_vals=None):
-        var = self._data[self._data["seq"] == seq]["residual"].apply(func).to_numpy()
-        print(f"- {name}:", np.array2string(var, precision=3, suppress_small=True))
+        residuals = self._data[self._data["seq"] == seq]["residual"]
+        residuals = residuals.apply(lambda x: x[1740:1800])
+        var = residuals.apply(func).to_numpy()
+        print(f"- {name}:", np.array2string(var, separator=", ", precision=3, suppress_small=True))
         if old_vals is not None:
             print(f"- original {name}:", np.array2string(old_vals, precision=3, suppress_small=True))
             print(f"- {name} difference:", np.array2string(var - old_vals, precision=3, suppress_small=True))
-            print(f"total diff: {(var - old_vals).sum():.3f}")
+            print(f"total {name} diff: {(var - old_vals).sum():.3f}")
 
     ###########
     # Utility #
