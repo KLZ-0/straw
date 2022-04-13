4c6f93899aac66cf098821822d0cf6458ec4f478
Adrián Kálazi <adrian@kalazi.com>
2022-04-13 20:02:33 +0200

    Format constants (#32)

--- src/straw/io/sizes.py
+++ src/straw/io/sizes.py
@@ -1,6 +1,53 @@
+UTF8 = None
+
+
 class StrawSizes:
     """
     Sizes for the straw format
+    Only for fields with at least one known dimension length
+    None for unknown values
     All sizes are in bits unless specified otherwise
     """
-    bps = 4
+
+    class metadata_block_header:  # noqa
+        last = 1
+        type = 7
+        size = 24
+
+    class metadata_block_streaminfo:  # noqa
+        samplerate = 20
+        channels = UTF8
+        bps = 5
+        frames = 27
+        samples = 36
+        md5 = 128
+        leading_channel = UTF8
+        shift = 4
+        removed_samples = None
+        bias = 8
+
+    class frame_header:  # noqa
+        sync_code = 14
+        contains_lpc = 1
+        block_size_length = 1
+        block_size_log2 = 8
+        block_size_exact = 16
+        lpc_order = 5
+        lpc_prec = 4
+        lpc_shift = 4
+        lpc_coeffs = None
+        frame_num = UTF8
+        frame_bytes = 32
+        crc = 8
+
+    class frame_footer:  # noqa
+        crc = 16
+
+    class subframe_header:  # noqa
+        type = 2
+
+    class subframe_lpc:  # noqa
+        is_coded = 1
+
+    class residual:  # noqa
+        param = 4
--- src/straw/io/straw.py
+++ src/straw/io/straw.py
@@ -5,6 +5,7 @@ from bitarray.util import int2ba
 from tqdm import tqdm
 
 from straw.io.base import BaseWriter, BaseReader
+from straw.io.sizes import StrawSizes
 
 
 class StrawFormatWriter(BaseWriter):
@@ -19,39 +20,42 @@ class StrawFormatWriter(BaseWriter):
 
     def _metadata_block(self):
         sec = self._metadata_block_header()
-        sec += self._metadata_block_data()
+        sec += self._metadata_block_streaminfo()
         sec.tofile(self._f)
 
     def _metadata_block_header(self) -> bitarray:
+        sizes = StrawSizes.metadata_block_header
         sec = bitarray()
         sec.append(1)  # this block is the last metadata block before the audio blocks
-        sec += int2ba(0, length=7)  # BLOCK_TYPE: STREAMINFO
+        sec += int2ba(0, length=sizes.type)  # BLOCK_TYPE: STREAMINFO
         # ignore if streaminfo...
         # sec += int2ba(data_size, length=24)
         return sec
 
-    def _metadata_block_data(self) -> bitarray:
+    def _metadata_block_streaminfo(self) -> bitarray:
+        sizes = StrawSizes.metadata_block_streaminfo
         sec = bitarray()
-        sec += int2ba(self._params.sample_rate, length=20)
+        sec += int2ba(self._params.sample_rate, length=sizes.samplerate)
         sec.frombytes(self.encode_int_utf8(self._params.channels - 1))
-        sec += int2ba(self._params.bits_per_sample - 1, length=5)
-        sec += int2ba(int(len(self._data[self._data["channel"] == 0])), length=27)
-        sec += int2ba(self._params.total_samples, length=36)
+        sec += int2ba(self._params.bits_per_sample - 1, length=sizes.bps)
+        sec += int2ba(int(len(self._data[self._data["channel"] == 0])), length=sizes.frames)
+        sec += int2ba(self._params.total_samples, length=sizes.samples)
         sec.frombytes(self._params.md5)
         sec.frombytes(self.encode_int_utf8(self._params.leading_channel))
         for val in self._params.lags:
-            sec += int2ba(int(val), length=4)
+            sec += int2ba(int(val), length=sizes.shift)
         for c in range(self._params.channels):
             for val in self._params.removed_samples_start[c]:
                 sec += int2ba(int(val), length=self._params.bits_per_sample, signed=True)
             for val in self._params.removed_samples_end[c]:
                 sec += int2ba(int(val), length=self._params.bits_per_sample, signed=True)
         for val in self._params.bias:
-            sec += int2ba(int(val), length=8, signed=True)
+            sec += int2ba(int(val), length=sizes.bias, signed=True)
         sec.fill()
         return sec
 
     def _frame(self, df: pd.DataFrame):
+        footer_sizes = StrawSizes.frame_footer
         sec = bitarray()
         qlp_idx = df["qlp"].first_valid_index()
         qlp = None if qlp_idx is None else df["qlp"][qlp_idx]
@@ -61,15 +65,16 @@ class StrawFormatWriter(BaseWriter):
         sec.fill()  # zero-padding to byte alignment
 
         # Header
-        sec = self._frame_header(df, len(sec.tobytes()) + 2) + sec
+        sec = self._frame_header(df, len(sec.tobytes()) + footer_sizes.crc // 8) + sec
 
         # Footer
-        sec += int2ba(self.Crc.crc16(sec.tobytes()), length=16)
+        sec += int2ba(self.Crc.crc16(sec.tobytes()), length=footer_sizes.crc)
         sec.tofile(self._f)
 
     def _frame_header(self, df: pd.DataFrame, frame_data_size: int) -> bitarray:
+        sizes = StrawSizes.frame_header
         sec = bitarray()
-        sec += int2ba(0b10101010101010, length=14)  # sync code
+        sec += int2ba(0b10101010101010, length=sizes.sync_code)  # sync code
         contains_lpc = df["qlp"].first_valid_index() is not None
         sec.append(contains_lpc)
 
@@ -80,10 +85,10 @@ class StrawFormatWriter(BaseWriter):
         tmp = np.log2(blocksize)
         if tmp % 1 == 0.0:
             sec.append(0)  # get 8 bit exponent for (2^n) samples
-            sec += int2ba(int(tmp), length=8)
+            sec += int2ba(int(tmp), length=sizes.block_size_log2)
         else:
             sec.append(1)  # get 16 bit (blocksize-1)
-            sec += int2ba(blocksize - 1, length=16)
+            sec += int2ba(blocksize - 1, length=sizes.block_size_exact)
         # block size end
 
         # LPC
@@ -92,16 +97,17 @@ class StrawFormatWriter(BaseWriter):
             qlp_precision = int(df["qlp_precision"][df["qlp_precision"].first_valid_index()])
             shift = int(df["shift"][df["shift"].first_valid_index()])
 
-            sec += int2ba(len(qlp) - 1, length=5)
-            sec += int2ba(qlp_precision - 1, length=4)
-            sec += int2ba(shift, length=4)
+            sec += int2ba(len(qlp) - 1, length=sizes.lpc_order)
+            sec += int2ba(qlp_precision - 1, length=sizes.lpc_prec)
+            sec += int2ba(shift, length=sizes.lpc_shift)
             for coeff in qlp:
                 sec += int2ba(int(coeff), length=qlp_precision, signed=True)
             sec.fill()
 
         sec.frombytes(self.encode_int_utf8(df.index.min()))
-        sec += int2ba(frame_data_size + len(sec.tobytes()) + 4 + 1, length=32)
-        sec += int2ba(self.Crc.crc8(sec.tobytes()), length=8)
+        sec += int2ba(frame_data_size + len(sec.tobytes()) + sizes.frame_bytes // 8 + sizes.crc // 8,
+                      length=sizes.frame_bytes)
+        sec += int2ba(self.Crc.crc8(sec.tobytes()), length=sizes.crc)
         return sec
 
     def _subframe(self, df: pd.Series, qlp: np.array) -> bitarray:
@@ -110,8 +116,9 @@ class StrawFormatWriter(BaseWriter):
         return sec
 
     def _subframe_header(self, df: pd.Series) -> bitarray:
+        sizes = StrawSizes.subframe_header
         sec = bitarray()
-        sec += int2ba(df["frame_type"], length=2)
+        sec += int2ba(df["frame_type"], length=sizes.type)
         return sec
 
     def _subframe_data(self, df: pd.Series, qlp: np.array) -> bitarray:
@@ -145,8 +152,9 @@ class StrawFormatWriter(BaseWriter):
         return sec
 
     def _residual(self, df: pd.Series) -> bitarray:
+        sizes = StrawSizes.residual
         sec = bitarray()
-        sec += int2ba(int(df["bps"]), length=4)
+        sec += int2ba(int(df["bps"]), length=sizes.param)
         sec += df["stream"]
         return sec
 
@@ -169,26 +177,28 @@ class StrawFormatReader(BaseReader):
 
     def _metadata_block(self) -> int:
         data_len = self._metadata_block_header()
-        return self._metadata_block_data()
+        return self._metadata_block_streaminfo()
 
     def _metadata_block_header(self) -> int:
+        sizes = StrawSizes.metadata_block_header
         last_metadata_block = self._sec.get_int()  # this block is the last metadata block before the audio blocks
-        block_type = self._sec.get_int(length=7)  # BLOCK_TYPE: STREAMINFO
+        block_type = self._sec.get_int(length=sizes.type)  # BLOCK_TYPE: STREAMINFO
         if block_type == 0:
             return 0
         else:
-            data_len = self._sec.get_int(length=24)
+            data_len = self._sec.get_int(length=sizes.size)
             return data_len
 
-    def _metadata_block_data(self) -> int:
-        self._params.sample_rate = self._sec.get_int(length=20)
+    def _metadata_block_streaminfo(self) -> int:
+        sizes = StrawSizes.metadata_block_streaminfo
+        self._params.sample_rate = self._sec.get_int(length=sizes.samplerate)
         self._params.channels = self._sec.get_int_utf8() + 1
         self._params.lags = np.zeros(self._params.channels, dtype=np.int8)  # 4 bit
         self._params.bias = np.zeros(self._params.channels, dtype=np.int8)  # 8 bit signed
-        self._params.bits_per_sample = self._sec.get_int(length=5) + 1
-        expected_frames = self._sec.get_int(length=27)
-        self._params.total_samples = self._sec.get_int(length=36)
-        self._params.md5 = self._sec.get_bytes(length=128)
+        self._params.bits_per_sample = self._sec.get_int(length=sizes.bps) + 1
+        expected_frames = self._sec.get_int(length=sizes.frames)
+        self._params.total_samples = self._sec.get_int(length=sizes.samples)
+        self._params.md5 = self._sec.get_bytes(length=sizes.md5)
 
         # Allocate sample buffer
         self._allocate_buffer(channels=self._params.channels,
@@ -197,7 +207,7 @@ class StrawFormatReader(BaseReader):
 
         self._params.leading_channel = self._sec.get_int_utf8()
         for i in range(self._params.lags.shape[0]):
-            self._params.lags[i] = self._sec.get_int(length=4)
+            self._params.lags[i] = self._sec.get_int(length=sizes.shift)
         total_size = self._samplebuffer.shape[1] - np.max(self._params.lags)
         for c in range(self._params.channels):
             lag = self._params.lags[c]
@@ -206,7 +216,7 @@ class StrawFormatReader(BaseReader):
             for e in range(total_size + lag, self._samplebuffer.shape[1]):
                 self._samplebuffer[c][e] = self._sec.get_int(length=self._params.bits_per_sample, signed=True)
         for i in range(self._params.bias.shape[0]):
-            self._params.bias[i] = self._sec.get_int(length=8, signed=True)
+            self._params.bias[i] = self._sec.get_int(length=sizes.bias, signed=True)
         self._sec.skip_padding()
 
         return expected_frames
@@ -222,35 +232,37 @@ class StrawFormatReader(BaseReader):
         self._sec.skip_padding()
 
         # Footer
+        footer_sizes = StrawSizes.frame_footer
         expected_crc = self.Crc.crc16(self._sec.get_from(start).tobytes())
-        checksum = self._sec.get_int(length=16)
+        checksum = self._sec.get_int(length=footer_sizes.crc)
         if expected_crc != checksum:
             raise RuntimeError(f"Inavalid frame checksum at frame {common['seq']}")
 
     def _frame_header(self) -> (dict, int, int):
+        sizes = StrawSizes.frame_header
         common = {}
         start = self._sec.get_pos()
-        if self._sec.get_int(length=14) != 0b10101010101010:  # sync code
+        if self._sec.get_int(length=sizes.sync_code) != 0b10101010101010:  # sync code
             raise RuntimeError("Lost sync")
         contains_lpc = self._sec.get_int()
         if self._sec.get_int():
-            blocksize = self._sec.get_int(length=16) + 1
+            blocksize = self._sec.get_int(length=sizes.block_size_exact) + 1
         else:
-            blocksize = 1 << self._sec.get_int(length=8)
+            blocksize = 1 << self._sec.get_int(length=sizes.block_size_log2)
 
         if contains_lpc:
-            order = self._sec.get_int(length=5) + 1
-            common["qlp_precision"] = self._sec.get_int(length=4) + 1
-            common["shift"] = self._sec.get_int(length=4)
+            order = self._sec.get_int(length=sizes.lpc_order) + 1
+            common["qlp_precision"] = self._sec.get_int(length=sizes.lpc_prec) + 1
+            common["shift"] = self._sec.get_int(length=sizes.lpc_shift)
             common["qlp"] = np.asarray(
                 [self._sec.get_int(length=common["qlp_precision"], signed=True) for _ in range(order)], dtype=np.int32)
             self._sec.skip_padding()
 
         common["seq"] = self._sec.get_int_utf8()
-        frame_size = self._sec.get_int(length=32)
+        frame_size = self._sec.get_int(length=sizes.frame_bytes)
 
         expected_crc = self.Crc.crc8(self._sec.get_from(start).tobytes())
-        checksum = self._sec.get_int(length=8)
+        checksum = self._sec.get_int(length=sizes.crc)
         if expected_crc != checksum:
             raise RuntimeError(f"Inavalid frame header checksum at frame {common['seq']}")
         return common, frame_size, blocksize
@@ -260,7 +272,8 @@ class StrawFormatReader(BaseReader):
         return self._subframe_data(subframe_type, order, blocksize, subframe_num)
 
     def _subframe_header(self) -> int:
-        return self._sec.get_int(length=2)
+        sizes = StrawSizes.subframe_header
+        return self._sec.get_int(length=sizes.type)
 
     def _subframe_data(self, subframe_type: int, order: int, blocksize: int, subframe_num: int) -> dict:
         if subframe_type == 0b00:  # SUBFRAME_CONSTANT
@@ -296,14 +309,16 @@ class StrawFormatReader(BaseReader):
                                                                                          self._params.lags[
                                                                                              subframe_num] + blocksize]
         }
-        row["was_coded"] = self._sec.get_int()
+        was_coded = self._sec.get_int()
+        row["was_coded"] = was_coded
         for i in range(order):
             row["frame"][i] = self._sec.get_int(length=self._params.bits_per_sample, signed=True)
         row["residual"], row["bps"] = self._residual(array=row["residual"])
         return row
 
     def _residual(self, array: np.array) -> np.array:
-        bps = self._sec.get_int(length=4)
+        sizes = StrawSizes.residual
+        bps = self._sec.get_int(length=sizes.param)
         bits_read = self._ricer.bitstream_to_frame(
             self._sec[self._sec.get_pos():self._sec.get_pos() + len(array) * self._params.bits_per_sample],
             len(array), bps, own_frame=array)
