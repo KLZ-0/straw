fdfbaf6ea4671b101da71d0bfd8f563e9fc42380
KLZ-0 <adrian@kalazi.com>
2022-04-13 16:15:40 +0200

    Modify print

--- src/straw/codec/encoder.py
+++ src/straw/codec/encoder.py
@@ -190,14 +190,15 @@ class Encoder(BaseCoder):
         :return: None
         """
         print(f"Number of frames: {len(self._data)}", file=stream)
-        print(f"Source size: {self._source_size} ({self._source_size / 2 ** 20:.2f} MiB)", file=stream)
+        print(f"Source size: {self._source_size} ({self._source_size / (2 ** 20):.2f} MiB)", file=stream)
         size = self._data["stream_len"].sum()
         print(f"md5: {self._params.md5.hex(' ')}", file=stream)
         print(f"Length of residual bitstream: {size} bits, "
-              f"bytes: {np.ceil(size / 8):.0f} aligned ({np.ceil(size / 8) / 2 ** 20:.2f} MiB)", file=stream)
+              f"bytes: {np.ceil(size / 8):.0f} aligned ({np.ceil(size / 8) / (2 ** 20):.2f} MiB)", file=stream)
         lpc_bytes = np.ceil(len(self._data) * self._lpc_precision * self._lpc_order * 1 / 8)
         print(f"Bytes needed for coefficients: {lpc_bytes:.0f} B", file=stream)
-        print(f"Output file size: {output_file.stat().st_size}", file=stream)
+        print(f"Output file size: {output_file.stat().st_size} ({output_file.stat().st_size / (2 ** 20):.2f} MiB)",
+              file=stream)
         print(f"Grand Ratio = {output_file.stat().st_size / self._source_size:.3f}", file=stream)
 
         # FIXME: this is misleading
