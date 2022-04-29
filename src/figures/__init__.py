355e459a5b9819f4796da0352612a73e34fa6456
KLZ-0 <adrian@kalazi.com>
2022-04-29 18:31:27 +0200

    Reorganize plots to be done with proper corrections

--- src/figures/__init__.py
+++ src/figures/__init__.py
@@ -10,26 +10,49 @@ def plot_all(args=None):
     from .lpc import LPCPlot
     from .rice import RicePlot
 
-    e = Encoder()
+    #######################
+    # Without corrections #
+    #######################
+
+    e = Encoder(do_corrections=())
     e.load_file(Path(args.input_files[0]))
     e.encode()
 
-    LPCPlot(e, args).print_lpc_and_qlp()
-    LPCPlot(e, args).prediction_comparison("prediction_comparison.pdf")
-    LPCPlot(e, args).residual("residual.pdf")
-    CorrectionsPlot(e, args).shift("shift.pdf")
-    CorrectionsPlot(e, args).gain("gain.pdf")
-    CorrectionsPlot(e, args).offset("offset.pdf")
-    CorrectionsPlot(e, args).all("all.pdf")
+    # CorrectionsPlot(e, args).shift("shift.pdf")
+    # CorrectionsPlot(e, args).gain("gain.pdf")
+    # CorrectionsPlot(e, args).offset("offset.pdf")
+    # CorrectionsPlot(e, args).all("all.pdf")
     CorrectionsPlot(e, args).real("corrected_before.pdf")
     CorrectionsPlot(e, args).real("corrected_shift.pdf", corrected=("shift",))
     CorrectionsPlot(e, args).real("corrected_gain.pdf", corrected=("gain",))
     CorrectionsPlot(e, args).real("corrected_bias.pdf", corrected=("bias",))
     CorrectionsPlot(e, args).real("corrected_all.pdf", corrected=("gain", "shift", "bias"))
-    LPCPlot(e, args).common_lpc_autoc_averaging("lpc_averaged.png")
-    LPCPlot(e, args).common_lpc_variances("lpc_averaged_variances.png")
-    RicePlot(e, args).interleave("rice_interleave.pdf")
-    RicePlot(e, args).static_m("rice_static_m.pdf")
-    RicePlot(e, args).dynamic_m("rice_dynamic_m.pdf")
-    RicePlot(e, args).k_diff("rice_k_diff.pdf")
-    RicePlot(e, args).rand_comparison("rice_rand_comparison.pdf")
+
+    ####################
+    # With corrections #
+    ####################
+
+    e = Encoder()
+    e.load_file(Path(args.input_files[0]))
+    e.encode()
+
+    # LPCPlot(e, args).print_lpc_and_qlp()
+    # LPCPlot(e, args).prediction_comparison("prediction_comparison.pdf")
+    # LPCPlot(e, args).residual("residual.pdf")
+    # LPCPlot(e, args).common_lpc_autoc_averaging("lpc_averaged.png")
+    # LPCPlot(e, args).common_lpc_variances("lpc_averaged_variances.png")
+    # RicePlot(e, args).interleave("rice_interleave.pdf")
+    # RicePlot(e, args).static_m("rice_static_m.pdf")
+    # RicePlot(e, args).dynamic_m("rice_dynamic_m.pdf")
+    # RicePlot(e, args).k_diff("rice_k_diff.pdf")
+    # RicePlot(e, args).rand_comparison("rice_rand_comparison.pdf")
+
+    ###########################
+    # With corrections + gain #
+    ###########################
+
+    e = Encoder(do_corrections=("gain", "shift", "bias"))
+    e.load_file(Path(args.input_files[0]))
+    e.encode()
+
+    CorrectionsPlot(e, args).real("corrected_all_global.pdf")
