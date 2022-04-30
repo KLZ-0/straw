effbbaa702a098818c073735d33db0fdb30d8416
KLZ-0 <adrian@kalazi.com>
2022-04-30 23:45:22 +0200

    Add option to keep the blocksizes power of two

--- src/straw/util/signals.py
+++ src/straw/util/signals.py
@@ -2,13 +2,43 @@ import librosa
 import numpy as np
 
 
-def resample_indices(raw_indices: np.array, min_block_size: int, max_block_size: int):
+def _find_nearest(array, value, offset=None):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    if offset is not None and not (array[idx] - offset <= value <= array[idx] + offset):
+        return value
+    return array[idx]
+
+
+def _find_valid_blocksizes(min_block_size, max_block_size) -> np.array:
+    blocksizes = []
+    i = 1
+    while i < min_block_size:
+        i <<= 1
+
+    while i <= max_block_size:
+        blocksizes.append(i)
+        i <<= 1
+
+    return np.asarray(blocksizes)
+
+
+def resample_indices(raw_indices: np.array,
+                     min_block_size: int,
+                     max_block_size: int,
+                     keep_blocksize_power_of_two: bool):
+    valid_blocksizes = _find_valid_blocksizes(min_block_size, max_block_size)
     indices = [0]
     currect_size = 0
     for i in range(1, raw_indices.shape[0]):
+        if raw_indices[i] <= indices[-1]:
+            continue
+
         currect_size += raw_indices[i] - raw_indices[i - 1]
         if min_block_size <= currect_size <= max_block_size:
-            indices.append(raw_indices[i])
+            if keep_blocksize_power_of_two:
+                currect_size = _find_nearest(valid_blocksizes, currect_size, offset=50)
+            indices.append(indices[-1] + currect_size)
             currect_size = 0
 
         while currect_size > max_block_size:
@@ -25,7 +55,8 @@ class Signals:
                                    min_block_size: int = 1 << 10,
                                    treshold: int = 62000,
                                    max_block_size: int = 1 << 12,
-                                   resolution: int = 10):
+                                   resolution: int = 10,
+                                   keep_blocksize_power_of_two: bool = True):
         """
         Returns a list of indices where frames should start
         """
@@ -47,7 +78,7 @@ class Signals:
         all_indices = borders.nonzero()[0] * resolution
         all_indices[-1] = channel_data.shape[0]
 
-        indices = resample_indices(all_indices, min_block_size, max_block_size)
+        indices = resample_indices(all_indices, min_block_size, max_block_size, keep_blocksize_power_of_two)
 
         return indices
 
